de922837e2bce1859c95cbded214834bf5ee77c6
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-25 15:10:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 10;
+let num4 = 6;
+let multiply = num1 * num2;
+console.log(multiply);
+let random = Math.floor(Math.random() * 10);
+let mod = num3 % num4;
+let max = Math.max(0,1, 2, 3, 4, 20, 10, 8);
